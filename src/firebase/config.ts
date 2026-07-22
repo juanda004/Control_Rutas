e97605437f82750144d3949a88c88931e8862be2
juanda004 <juandavid3487@gmail.com>
2026-07-21 20:24:03 -0500

    fix: update Firebase API key in config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-6156191849-90361",
   "appId": "1:980853525745:web:9e254784edf332a130ed50",
-  "apiKey": "AIzaSyD-n4v-flobKeug4OOiHm_Ldhhu9teIZSI",
+  "apiKey": "AIzaSyCqZFuB17TibCI-LYmibvsHcKsvlvGI1o0",
   "authDomain": "studio-6156191849-90361.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "980853525745"
